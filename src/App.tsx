a530269f6b1f101b5860eded41bc2e6e26200b32
DerickSena <derick_sena13@outlook.com>
2023-12-06 16:24:43 -0300

    feat: menu lateral e resposividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 /* eslint-disable linebreak-style */
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
-import { AppThemeProvider } from './shared/contexts';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-        <MenuLateral>
-          <AppRoutes />
-        </MenuLateral>
-      </BrowserRouter>
-    </AppThemeProvider>
+      <DrawerProvider>
+        <BrowserRouter>
 
+          <MenuLateral>
+            <AppRoutes />
+          </MenuLateral>
+          
+        </BrowserRouter>
+      </DrawerProvider>
+    </AppThemeProvider>
 
   );
 };
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,17 +1,22 @@
-import { Avatar, Divider, Drawer , Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme} from '@mui/material';
+import { Avatar, Divider, Drawer , Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme} from '@mui/material';
 import { Box} from '@mui/system';
+import { useDrawerContext } from '../../contexts';
 interface IMenuLateral{
     children: React.ReactNode
 }
 
 export const MenuLateral:React.FC<IMenuLateral> = ({children})=>{
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const {isDrawerOpen,toggleDrawerOpen } = useDrawerContext();
+
   return (
     <>
-      <Drawer variant='temporary'>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column"  >
           
-          <Box width="100%" height={theme.spacing(15)} display="flex" alignItems="center" justifyContent="center">
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar 
               sx={{height:theme.spacing(12) , width: theme.spacing(12)}}
               src="https://i.pinimg.com/564x/f2/99/34/f29934072952b077ca21618afb1122f0.jpg"
@@ -36,7 +41,7 @@ export const MenuLateral:React.FC<IMenuLateral> = ({children})=>{
         </Box>
         
       </Drawer>
-      <Box height="100vh"  marginLeft={theme.spacing(177)}>
+      <Box height="100vh"  marginLeft={smDown ? 0 :theme.spacing(28)}>
         {children}
       </Box>
         
