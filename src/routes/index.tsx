8fb274023fd82ba951d88e0d64688d4b74e4021d
DerickSena <derick_sena13@outlook.com>
2023-12-07 11:56:52 -0300

    feat: Base de Pagina Inicial

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
-import { Button } from '@mui/material';
+
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 import { useEffect } from 'react';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen,setDrawerOptions } = useDrawerContext();
+  const {setDrawerOptions } = useDrawerContext();
   
   useEffect(() => {
     setDrawerOptions([
       {
         label:'Página Inicial',
-        icon:'icon',
+        icon:'home',
         path:'/pagina-inicial',
       },
     ]);
@@ -18,7 +19,7 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}> Toggle drawer </Button>} />
+      <Route path="/pagina-inicial" element={<Dashboard/>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
